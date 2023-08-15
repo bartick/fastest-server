7a669067183f7efb8ffdafc615f0eeca362d3861
bartick <bartickmaiti1998@gmail.com>
2023-08-15 17:59:54 +0530

    add: rust

---
+++ RUST/src/main.rs
@@ -0,0 +1,35 @@
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    println!("Connection established!");
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:?}", http_request[0]);
+
+    let status_line = "HTTP/1.1 200 OK";
+    let request_path = http_request[0].split_whitespace().collect::<Vec<_>>()[1];
+    let contents = format!("Hello World!\nRequest: {request_path}") ;
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
+}
